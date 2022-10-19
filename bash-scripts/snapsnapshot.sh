2c240c6b4e1a2c21e1152ee5de5b3b3f901dc22c
scuba <54933878+scubamuc@users.noreply.github.com>
2022-10-19 13:44:57 +0200

    Create snapsnapshot.sh

---
+++ bash-scripts/snapsnapshot.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+##############################################################
+## Nextcloud-snap backup with snap snapshot -- SCUBA --
+##############################################################
+## create target directory "sudo mkdir /media/SNAPBACKUP"
+## snapshot rotation 30 days 
+## create crontab as root for automation
+##############################################################
+# VARIABLES #
+##############################################################
+
+SNAPNAME="nextcloud"
+TARGET="/media/SNAPBACKUP"  ## target directory
+LOG="/media/SNAPBACKUP/snapbackup-nc.log"  ## logfile
+SOURCE="/var/lib/snapd/snapshots" ## source directory
+RETENTION="30" ## retention in days
+
+##############################################################
+# FUNCTIONS #
+##############################################################
+
+## Timestamp für Log ##
+timestamp()
+{
+ date +"%Y-%m-%d %T"
+}
+
+##############################################################
+# SCRIPT #
+##############################################################
+
+## start log  
+echo "********************************************************" >> "$LOG" ; ## log seperator
+echo "$(timestamp) -- Snapbackup $SNAPNAME Start" >> "$LOG" ; ## start log
+
+## stop snap for snapshot  
+sudo snap stop $SNAPNAME ;
+## create snap snapshot 
+sudo snap save --abs-time $SNAPNAME ;
+## restart snap after snapshot 
+sudo snap start $SNAPNAME ;
+
+## find and move snapshot to $TARGET  
+sudo find $SOURCE/ -name "*.zip" -exec mv '{}' $TARGET/ \; # find and move
+## find old snapshots and delete snapshots older than $RETENTION days
+sudo find $TARGET/ -name "*.zip" -mtime +$RETENTION -exec rm -f {} \; # find and delete
+
+## end log 
+echo "$(timestamp) -- Snapbackup $SNAPNAME End " >> "$LOG" ; ## end log 
+echo "" >> "$LOG" ;  ## log linefeed 
+
+exit
