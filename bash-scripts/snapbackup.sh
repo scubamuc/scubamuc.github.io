5a7ce2bfb44790924fd52ac27b707570e807e41a
scuba <54933878+scubamuc@users.noreply.github.com>
2022-10-19 13:42:30 +0200

    Create snapbackup.sh

---
+++ bash-scripts/snapbackup.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+##############################################################
+
+## Nextcloud-snap backup with nextcloud.export -- SCUBA --
+
+##############################################################
+
+## backup directory: '/var/snap/nextcloud/common/backups'
+## restore directory: '/var/snap/nextcloud/common'
+## backup rotation 30 days
+## create crontab as root for automation
+
+##############################################################
+
+# VARIABLES #
+
+##############################################################
+
+SNAPNAME="nextcloud"
+BACKUPNAME="mybackup"
+TARGET="/media/SNAPBACKUP" ## target directory
+SOURCE="/var/snap/nextcloud/common/backups" ## source directory
+RETENTION="30" ## file retention in days
+
+##############################################################
+
+# SCRIPT #
+
+##############################################################
+
+## create backup with nextcloud.export ##
+sudo nextcloud.export ; ## nextcloud.export, see options
+
+## find and compress backup directory ##
+list=`ls $SOURCE`
+for i in $list
+do
+    if [ ! "$i" == "." ]; then
+      sudo tar -czf ${i}-$BACKUPNAME.tar.gz ${i}
+    fi
+done
+
+## find and move compressed backup file to $TARGET
+sudo find $SOURCE/ -name "*.tar.gz" -exec mv '{}' $TARGET/ \;
+
+## find and rotate/delete old backups
+sudo find $ZIEL/ -name "*.tar.gz" -mtime +$RETENTION -exec rm -f {} \; 
+
+exit
