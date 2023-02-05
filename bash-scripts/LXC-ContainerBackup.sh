c43fac5deea9b95dd1e26ad65d1679ed28e38c67
scuba <54933878+scubamuc@users.noreply.github.com>
2023-02-05 11:47:12 +0100

    Add files via upload

---
+++ bash-scripts/LXC-ContainerBackup.sh
@@ -0,0 +1,61 @@
+  #!/bin/bash
+##############################################################
+## -- SCUBA --
+## LXC container incremental backup image to target directory
+##############################################################
+## create target directory
+## create crontab for automation
+## crontab execution hourly/daily/weekly
+## retention in days
+##############################################################
+# VARIABLES #
+##############################################################
+
+CONTAINERNAME="NEXTCLOUD" ## LXC Containername
+DATE="`date +"%Y%m%d"`"
+IMAGENAME="$CONTAINERNAME-$DATE"
+TARGET="/media/EXT-HDD/LXC-BACKUP"  ## Target directory
+LOG="$TARGET/$CONTAINERNAME--LXC-BACKUP.log"  ## Log file
+RETENTION="30" ## Retention in days
+
+##############################################################
+# FUNCTIONS #
+##############################################################
+
+## Timestamp for Log ##
+timestamp()
+{
+ date +"%Y-%m-%d %T"
+}
+
+##############################################################
+# SCRIPT #
+##############################################################
+
+## start log
+echo "***********************************************************" >> "$LOG" ; ## log seperator
+echo "$(timestamp) -- LXC-Backup $IMAGENAME Start" >> "$LOG" ; ## start log
+
+## Container snapshot
+lxc snapshot $CONTAINERNAME/$CONTAINERNAME --reuse ;
+## publish Container with Alias
+lxc publish $CONTAINERNAME/$CONTAINERNAME --alias $CONTAINERNAME ;
+## create image from snapshot
+lxc image export $CONTAINERNAME $TARGET/$IMAGENAME ;
+## delete Image on Host
+lxc image delete $CONTAINERNAME ;
+
+## find old Images and delete older than $RETENTION days
+find $TARGET/ -name "*.tar.gz" -mtime +$RETENTION -exec rm -f {} \; # find and delete
+
+## end log
+echo "$(timestamp) -- LXC-Backup $IMAGENAME End  " >> "$LOG" ; ## end log
+echo "" >> "$LOG" ;  ## log linefeed
+
+## Restore Image
+## To restore and create a container from it, you can then do:
+## lxc image import $TARGET/$IMAGENAME.tar.gz --alias $IMAGENAME'
+## lxc launch $IMAGENAME $CONTAINERNAME
+## lxc image delete $IMAGENAME
+
+exit
