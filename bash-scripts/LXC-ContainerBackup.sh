0b53c4f91342d4cbc564662e3a5b91d9cd857707
scubamuc <54933878+scubamuc@users.noreply.github.com>
2023-09-08 14:15:40 +0200

    Update LXC-ContainerBackup.sh

--- bash-scripts/LXC-ContainerBackup.sh
+++ bash-scripts/LXC-ContainerBackup.sh
@@ -2,6 +2,7 @@
 ##############################################################
 ## -- SCUBA --
 ## LXC container incremental backup image to target directory
+## Script -scubamuc- https://scubamuc.github.io/ 
 ##############################################################
 ## create target directory
 ## create crontab for automation
@@ -23,34 +24,34 @@ RETENTION="30" ## Retention in days
 ##############################################################
 
 ## Timestamp for Log ##
-timestamp()
-{
- date +"%Y-%m-%d %T"
-}
+  timestamp()
+  {
+  date +"%Y-%m-%d %T"
+  }
 
 ##############################################################
 # SCRIPT #
 ##############################################################
 
 ## start log
-echo "***********************************************************" >> "$LOG" ; ## log seperator
-echo "$(timestamp) -- LXC-Backup $IMAGENAME Start" >> "$LOG" ; ## start log
+  echo "***********************************************************" >> "$LOG" ; ## log seperator
+  echo "$(timestamp) -- LXC-Backup $IMAGENAME Start" >> "$LOG" ; ## start log
 
 ## Container snapshot
-lxc snapshot $CONTAINERNAME/$CONTAINERNAME --reuse ;
+  lxc snapshot $CONTAINERNAME/$CONTAINERNAME --reuse ;
 ## publish Container with Alias
-lxc publish $CONTAINERNAME/$CONTAINERNAME --alias $CONTAINERNAME ;
+  lxc publish $CONTAINERNAME/$CONTAINERNAME --alias $CONTAINERNAME ;
 ## create image from snapshot
-lxc image export $CONTAINERNAME $TARGET/$IMAGENAME ;
+  lxc image export $CONTAINERNAME $TARGET/$IMAGENAME ;
 ## delete Image on Host
-lxc image delete $CONTAINERNAME ;
+  lxc image delete $CONTAINERNAME ;
 
 ## find old Images and delete older than $RETENTION days
-find $TARGET/ -name "*.tar.gz" -mtime +$RETENTION -exec rm -f {} \; # find and delete
+  find $TARGET/ -name "*.tar.gz" -mtime +$RETENTION -exec rm -f {} \; # find and delete
 
 ## end log
-echo "$(timestamp) -- LXC-Backup $IMAGENAME End  " >> "$LOG" ; ## end log
-echo "" >> "$LOG" ;  ## log linefeed
+  echo "$(timestamp) -- LXC-Backup $IMAGENAME End  " >> "$LOG" ; ## end log
+  echo "" >> "$LOG" ;  ## log linefeed
 
 ## Restore Image
 ## To restore and create a container from it, you can then do:
