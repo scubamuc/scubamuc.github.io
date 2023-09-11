6ee03935b253f6e275f5a8d80a320fb8a22025f9
scubamuc <54933878+scubamuc@users.noreply.github.com>
2023-09-11 13:53:57 +0200

    Update sysinfo_en.sh

--- bash-scripts/sysinfo_en.sh
+++ bash-scripts/sysinfo_en.sh
@@ -85,7 +85,7 @@ echo ''
 echo '       ==================================   '
 echo '       Memory and disk usage overview!            '
 echo ''
-        sudo lsblk -e7 -o NAME,SIZE,FSUSED,FSUSE%,FSAVAIL # View disk usage ignoring loop | grep -v "loop"
+        sudo lsblk -e7 -o NAME,SIZE,FSUSED,FSUSE%,FSAVAIL # View disk usage ignoring loop | grep -v "ram"
 echo ''
 read -p "  Enter to continue... Crtl+c to close..."
 	clear
