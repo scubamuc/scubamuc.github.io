f3e594d6bd78f5d2bab60c90121a27fc525be8e2
scubamuc <54933878+scubamuc@users.noreply.github.com>
2023-10-22 12:28:02 +0200

    Update enable-nextcloud-office.sh

--- bash-scripts/enable-nextcloud-office.sh
+++ bash-scripts/enable-nextcloud-office.sh
@@ -2,21 +2,21 @@
 ##############################################################
 # Script description  -scubamuc- https://scubamuc.github.io/ #
 # Re-enable Nextcloud office & CODE on Nextcloud snap        #
-# This skript re-enables Nextcloud office and CODE on        #
+# This script re-enables Nextcloud office and CODE on        #
 # Nextcloud snap installation, where Nextcloud office        #
 # loses or changes its wonrking configuration after an       #
 # app-update or app-upgrade                                  #
 ##############################################################
 # 1. Disable CODE
-sudo nextcloud.occ app:disable richdocumentscode ;
+  sudo nextcloud.occ app:disable richdocumentscode ;
 # 2. Disable Nextcloud office
-sudo nextcloud.occ app:disable richdocuments ;
+  sudo nextcloud.occ app:disable richdocuments ;
 # 3. Delete public_wopi configuration
-sudo nextcloud.occ config:app:delete richdocuments public_wopi_url ;
+  sudo nextcloud.occ config:app:delete richdocuments public_wopi_url ;
 # 4. Delete wopi_url configuration
-sudo nextcloud.occ config:app:delete richdocuments wopi_url ;
+  sudo nextcloud.occ config:app:delete richdocuments wopi_url ;
 # 5. Enable Nextcloud office
-sudo nextcloud.occ app:enable richdocuments ;
+  sudo nextcloud.occ app:enable richdocuments ;
 # 6. Enable CODE
-sudo nextcloud.occ app:enable richdocumentscode ;
+  sudo nextcloud.occ app:enable richdocumentscode ;
 exit
