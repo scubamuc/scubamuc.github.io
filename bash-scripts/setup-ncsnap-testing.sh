2f15c97ee76ef3aa0be5272f5732cf176c7f41a6
scubamuc <54933878+scubamuc@users.noreply.github.com>
2023-10-27 11:21:39 +0200

    Add files via upload

---
+++ bash-scripts/setup-ncsnap-testing.sh
@@ -0,0 +1,40 @@
+  #!/bin/bash
+##############################################################
+# Setup Nextcloud snap test instance
+##############################################################
+# backup working config.php
+	sudo cp /var/snap/nextcloud/current/nextcloud/config/config.php /var/snap/nextcloud/current/nextcloud/config/config.php.bak ;
+# set overwrite.cli.url
+	sudo nextcloud.occ config:system:set overwrite.cli.url --value="https://cloud.yourdomain.com" ;
+# set trusted proxy
+	sudo nextcloud.occ config:system:set trusted_poxies 0 --value="your.proxy.ip" ;
+# set default phone region
+	sudo nextcloud.occ config:system:set default_phone_region --value="US" ;
+# set http compression
+	sudo snap set nextcloud http.compression=true ;
+## Set Email
+	sudo nextcloud.occ config:system:set mail_from_address --value="cloud.admin" ;
+	sudo nextcloud.occ config:system:set mail_smtpmode --value="smtp" ;
+	sudo nextcloud.occ config:system:set mail_sendmailmode --value="smtp" ;
+	sudo nextcloud.occ config:system:set mail_domain --value="your.domain.com" ;
+	sudo nextcloud.occ config:system:set mail_smtpauth --value="1" ;
+	sudo nextcloud.occ config:system:set mail_smtphost --value="your.smpt.host" ;
+	sudo nextcloud.occ config:system:set mail_smtpport --value="port" ;
+	sudo nextcloud.occ config:system:set mail_smtpname --value="smtpusername" ;
+	sudo nextcloud.occ config:system:set mail_smtppassword --value="smtppassword" ;
+	sudo nextcloud.occ config:system:set mail_smtpsecure --value="smtpsecurity" ;
+## CODE & Office settings
+# 1. Disable CODE
+	sudo nextcloud.occ app:disable richdocumentscode ;
+# 2. Disable Nextcloud office
+	sudo nextcloud.occ app:disable richdocuments ;
+# 3. Delete public_wopi configuration
+	sudo nextcloud.occ config:app:delete richdocuments public_wopi_url ;
+# 4. Delete wopi_url configuration
+	sudo nextcloud.occ config:app:delete richdocuments wopi_url ;
+# 5. Enable Nextcloud office
+	sudo nextcloud.occ app:enable richdocuments ;
+# 6. Enable CODE
+	sudo nextcloud.occ app:enable richdocumentscode ;
+
+exit
