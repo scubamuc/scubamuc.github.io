72c4b34b68952d2736350b45e4a8a80957610cc0
scubamuc <54933878+scubamuc@users.noreply.github.com>
2023-12-11 14:18:35 +0100

    Update setup-ncsnap-testing.sh

--- bash-scripts/setup-ncsnap-testing.sh
+++ bash-scripts/setup-ncsnap-testing.sh
@@ -1,15 +1,17 @@
-  #!/bin/bash
+ #!/bin/bash
 ##############################################################
-## -- SCUBA --
-## Script -scubamuc- https://scubamuc.github.io/ 
+# Script description  -scubamuc- https://scubamuc.github.io/ #
 ##############################################################
-##############################################################
-# Setup Nextcloud snap test instance
+# Setup Nextcloud snap testing environment for nextcloud-snap
+# This script assumes running your test instance behind a 
+# reverse proxy managing encryption
 ##############################################################
 # backup working config.php
 	sudo cp /var/snap/nextcloud/current/nextcloud/config/config.php /var/snap/nextcloud/current/nextcloud/config/config.php.bak ;
 # set overwrite.cli.url
 	sudo nextcloud.occ config:system:set overwrite.cli.url --value="https://cloud.yourdomain.com" ;
+# set overwrite host protocol
+	sudo nextcloud.occ config:system:set overwriteprotocol --value="https" ;
 # set trusted proxy
 	sudo nextcloud.occ config:system:set trusted_poxies 0 --value="your.proxy.ip" ;
 # set default phone region
@@ -26,7 +28,7 @@
 	sudo nextcloud.occ config:system:set mail_smtpport --value="port" ;
 	sudo nextcloud.occ config:system:set mail_smtpname --value="smtpusername" ;
 	sudo nextcloud.occ config:system:set mail_smtppassword --value="smtppassword" ;
-	sudo nextcloud.occ config:system:set mail_smtpsecure --value="smtpsecurity" ;
+	sudo nextcloud.occ config:system:set mail_smtpsecure --value="ssl" ;
 ## CODE & Office settings
 # 1. Disable CODE
 	sudo nextcloud.occ app:disable richdocumentscode ;
@@ -40,5 +42,4 @@
 	sudo nextcloud.occ app:enable richdocuments ;
 # 6. Enable CODE
 	sudo nextcloud.occ app:enable richdocumentscode ;
-
 exit
